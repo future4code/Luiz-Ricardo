9b946e0c8a45ffca48406d4c1d99a3c608cf7a55
Luiz Ricardo da Silva <luiz.dug@gmail.com>
2021-10-05 20:04:01 -0300

    Aula 32 - Hook useEffect()

---
+++ semana9/aula32-use-effect/src/App.js
@@ -0,0 +1,48 @@
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import PokeCard from "./Components/PokeCard/PokeCard";
+import './style.css';
+
+
+function App() {
+
+  const [pokeList, setPokeList] = useState ([])
+  const [pokeName, setPokeName] = useState ("")
+
+  const getPokemons = () => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon/?limit=151")
+      .then(response => {
+        setPokeList(response.data.results)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  useEffect(() => {
+    getPokemons()
+  }, [])
+
+  const changePokeName = event => {
+    setPokeName(event.target.value)
+  }
+
+  return (
+    <div className="App">
+      <select onChange={changePokeName}>
+        <option value={""}>Nenhum</option>
+        {pokeList.map(pokemon => {
+          return (
+            <option key={pokemon.name} value={pokemon.name}>
+              {pokemon.name}
+            </option>
+          )
+        })}
+      </select>
+      {pokeName && <PokeCard pokemon={pokeName} />}
+    </div>
+  );
+}
+
+export default App;
