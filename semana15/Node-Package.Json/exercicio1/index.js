7ba8947094a4049e51d63da0a2e346c14f946713
Luiz Ricardo da Silva <luiz.dug@gmail.com>
2021-11-19 14:51:11 -0300

    Aula 41 - Node e Package.json

--- semana15/Node-Package.Json/exercicio1/index.js
+++ semana15/Node-Package.Json/exercicio1/index.js
@@ -1,15 +1,15 @@
 //Exercício A:
-//Podemos acessar os parametros passados na linha de comando pelo process.argv que percore um array.
+//Podemos acessar os parametros passados na linha de comando pelo process.argv.
 
 //Exercício B:
-const nome = process.argv[2],
-const idade = Number(process.argv[3])
+// const nome = process.argv[2]
+// const idade = Number(process.argv[3])
 
-console.log(`Olá, ${nome}! Você tem ${idade} anos.`)
+// console.log(`Olá, ${nome}! Você tem ${idade} anos.`)
 
 //Exercício C:
-const nome = process.argv[2],
+const nome = process.argv[2]
 const idade = Number(process.argv[3])
 const idadeMaisSete = idade + 7 
 
-console.log(`Olá, ${nome}! Você tem ${idade} anos. Em sete anos você terá ${idadeMaisSete}`)
+console.log(`Olá, ${nome}! Você tem ${idade} anos. Em sete anos você terá ${idadeMaisSete}.`)
