2982d6a61409cebc6cc148332b836b38643bbc14
Luiz Ricardo da Silva <luiz.dug@gmail.com>
2021-11-27 14:09:02 -0300

    Aula 43 - Express.js

--- semana15/express.js/src/index.ts
+++ semana15/express.js/src/index.ts
@@ -1,11 +1,98 @@
 import express from "express"
 import cors from "cors"
 import { countries } from "./data"
+import { country } from "./types"
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+
+//ENDPOINT 1 - BUSCAR TODOS OS PAÍSES
+
+app.get("/countries", (req, res) => {
+    
+    const result = countries.map(country => ({
+        id: country.id,
+        name: country.name
+    }))
+    
+    res.status(200).send(result)
+})
+
+//ENDPOINT 3 - BUSCAR COM FILTROS
+
+app.get("/countries/search", (req, res) => {
+    
+    let result: country[] = countries
+
+        if (req.query.name) {
+            result = result.filter(
+                country => country.name.includes(req.query.name as string)
+            )
+        }
+
+        if (req.query.capital) {
+            result = result.filter(
+                country => country.capital.includes(req.query.capital as string)
+            )
+        }
+
+        if (req.query.continent) {
+            result = result.filter(
+                country => country.continent.includes(req.query.continent as string)
+            )
+        } else {
+            res.status(404).send("País não encontrado")
+        }
+
+    res.status(200).send(result)
+})
+
+//ENDPOINT 2 - BUSCAR PAÍS POR ID
+
+app.get("/countries/:id",
+    (req, res) => {
+        const id = req.params.id
+
+        const result = countries.find((country) => {
+            return country.id === Number(id)
+        })
+
+        if (result) {
+            res.status(200).send(result)
+        } else {
+            res.status(404).send("País não encontrado!")
+        }
+    }
+)
+
+//ENDPOINT 4 - EDITAR PAÍS
+
+app.put("/countries/:id", (req, res) => {
+    try {
+        const id = req.params.id
+    
+        let result = countries.find((country) => country.id === Number(id))
+    
+        const newResult = {
+          id: result?.id,
+          name: req.body.name,
+          capital: req.body.capital,
+          continent: result?.continent,
+        }
+    
+        if (result === undefined) {
+          throw new Error("Nenhum resultado foi encontrado")
+        }
+    
+        result.name = newResult.name
+    
+        res.status(200).send(newResult)
+    } catch (erro: any) {
+        res.status(400).send({ message: erro.message })
+    }
+})    
 
 app.listen(3003, () => {
     console.log("Servidor Pronto!")
