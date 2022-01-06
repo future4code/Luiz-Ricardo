f5474482f561786a22adcdc0b393c239680e9add
Luiz Ricardo da Silva <luiz.dug@gmail.com>
2022-01-06 13:17:25 -0300

    Aula 51 - Serviços no Backend

--- semana17/servicos-backend/src/index.ts
+++ semana17/servicos-backend/src/index.ts
@@ -1,1 +1,7 @@
-console.log('Partiu Aula 51!')
+import { app } from "./app"
+import { getAddressInfo } from "./services/getAddressInfo"
+import createAddress from "./endpoint/createAddress"
+
+getAddressInfo("05001200", 1705, "Casa do Palmeiras").then(console.log)
+
+app.post("/address/signup", createAddress)
